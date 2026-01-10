d0ce7973bdc8595df888eb2308dce3c7f22d7332
liuhuan <crane0927@163.com>
2026-01-10 17:36:48 +0800

    feat(config): 新增配置工厂模式实现

---
+++ src/main/java/com/example/factory/configfactory/Coffee.java
@@ -0,0 +1,8 @@
+package com.example.factory.configfactory;
+
+/**
+ * Coffee.
+ */
+public interface Coffee {
+    String name();
+}
---
+++ src/main/java/com/example/factory/configfactory/Latte.java
@@ -0,0 +1,11 @@
+package com.example.factory.configfactory;
+
+/**
+ * Latte.
+ */
+public class Latte implements Coffee {
+    @Override
+    public String name() {
+        return "Latte";
+    }
+}
