9dc96fd5e5567da0f23ffbc0df7af034d88b1cea
liuhuan <crane0927@163.com>
2026-01-12 00:36:12 +0800

    docs(adapter): 更新SDAdapterTF类的文档注释，增加适配器模式说明

--- src/main/java/com/example/structural/adapter/class_adapter/SDAdapterTF.java
+++ src/main/java/com/example/structural/adapter/class_adapter/SDAdapterTF.java
@@ -3,6 +3,7 @@ package com.example.structural.adapter.class_adapter;
 import com.example.structural.adapter.class_adapter.impl.TFCardImpl;
 
 /**
+ * 类适配器，通过继承的方式实现适配器模式
  * 适配 TF 卡
  * 让计算机和读取 SD 卡一样的读取 TF 卡 违反合成复用原则
  */
