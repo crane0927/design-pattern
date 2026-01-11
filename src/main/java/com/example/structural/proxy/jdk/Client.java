94e0c9191e6c5812828fcfb5c41bea45df21760f
liuhuan <crane0927@163.com>
2026-01-11 22:06:48 +0800

    docs(proxy): 添加 jdk 动态代理执行流程和代理类说明注释

--- src/main/java/com/example/structural/proxy/jdk/Client.java
+++ src/main/java/com/example/structural/proxy/jdk/Client.java
@@ -3,6 +3,13 @@ package com.example.structural.proxy.jdk;
 public class Client {
 
     public static void main(String[] args) {
+        /*
+        动态代理执行流程
+        1. 通过代理对象调用 sell() 方法
+        2. 由于 sellTickets 指向的是动态生成的代理对象，所以会执行代理类中的 sell() 方法
+        3. 代理类中的 sell() 方法会调用传入的 InvocationHandler 实现类（匿名内部类）的 invoke() 方法
+        4. invoke() 方法通过反射执行真实对象 Station 中的 sell() 方法
+        */
         ProxyFactory proxyFactory = new ProxyFactory(new Station());
         SellTickets sellTickets = proxyFactory.getSellTickets();
         sellTickets.sell(); // 执行被代理对象的 sell 方法
--- src/main/java/com/example/structural/proxy/jdk/ProxyFactory.java
+++ src/main/java/com/example/structural/proxy/jdk/ProxyFactory.java
@@ -15,6 +15,14 @@ public class ProxyFactory {
         this.station = station;
     }
 
+    /*
+    动态代理类是在内存中的，ProxyFactory 不是代理类，只是一个代理工厂
+    动态代理类会继承 Proxy 类，被代理对象 Station 实现了 SellTickets，动态代理类也实现了 SellTickets
+    动态代理类都有参构造接收匿名内部类 InvocationHandler ，并赋值给 Proxy 中的属性 protected InvocationHandler h 
+    动态代理类调用 sell() 方法，sell() 方法中又调用了 InvocationHandler 的 invoke 方法
+    invoke 方法 通过反射执行类真实对象所属类【Station】中的 sell() 方法
+    */
+
     public SellTickets getSellTickets(){
         // 返回代理对象
 
@@ -29,7 +37,7 @@ public class ProxyFactory {
         new InvocationHandler(){
             /**
              *  proxy 被代理的对象，等同于 newProxyInstance
-             *  method 被代理的对象的方法中的方法
+             *  method 被调用的方法对象
              *  args 方法的执行参数
              */
             @Override
